c723a78770e648cdcc0dce7f20b9f13187b05549
POOJARANGAN <122766925+POOJARANGAN@users.noreply.github.com>
2023-03-19 09:07:52 -0700

    hw7

---
+++ assignments/07_syndna/syndna.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+"""
+Author : poojarangan <poojarangan@localhost>
+Date   : 2023-03-17
+Purpose: Create synthetic sequences
+"""
+
+import argparse
+import random
+
+
+def create_pool(pctgc, max_len, seq_type):
+    """ Create the pool of bases """
+
+    t_or_u = 'T' if seq_type == 'dna' else 'U'
+    num_gc = int((pctgc / 2) * max_len)
+    num_at = int(((1 - pctgc) / 2) * max_len)
+    pool = 'A' * num_at + 'C' * num_gc + 'G' * num_gc + t_or_u * num_at
+
+    for _ in range(max_len - len(pool)):
+        pool += random.choice(pool)
+
+    return ''.join(sorted(pool))
+
+# --------------------------------------------------
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Create synthetic sequences',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output filename',
+                        metavar='str',
+                        type=argparse.FileType('wt'),
+                        default='out.fa')
+
+    parser.add_argument('-t',
+                        '--seqtype',
+                        help='DNA or RNA',
+                        metavar='str',
+                        type=str,
+                        default='dna',
+                        choices=['dna', 'rna'])
+
+    parser.add_argument('-n',
+                        '--numseq',
+                        help='Number of sequences to create',
+                        metavar='int',
+                        type=int,
+                        default=10)
+
+    parser.add_argument('-m',
+                        '--minlen',
+                        help='Minimum length',
+                        metavar='int',
+                        type=int,
+                        default=50)
+
+    parser.add_argument('-x',
+                        '--maxlen',
+                        help='Maximum length',
+                        metavar='int',
+                        type=int,
+                        default=75)
+
+    parser.add_argument('-p',
+                        '--pctgc',
+                        help='Percent GC',
+                        metavar='float',
+                        type=float,
+                        default=0.5)
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Minimum length',
+                        metavar='int',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if not 0 < args.pctgc < 1:
+        parser.error(f'--pctgc "{args.pctgc}" must be between 0 and 1')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    pool = create_pool(args.pctgc, args.maxlen, args.seqtype)
+
+    with args.outfile as outfile:
+        for i in range(args.numseq):
+            seq_len = random.randint(args.minlen, args.maxlen)
+            seq = ''.join(random.sample(pool, seq_len))
+            outfile.write(f'>seq_{i+1}\n{seq}\n')
+
+    print(
+        f'Done, wrote {args.numseq} {args.seqtype.upper()}'
+        f' sequences to "{args.outfile.name}".')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
